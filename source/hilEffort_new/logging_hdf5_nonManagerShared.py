c010e1629699bdbc01b77acd56fe9a58455ca0bd
Md Titoo <titooeee@gmail.com>
2024-10-19 22:12:58 -0700

    WIP

--- source/hilEffort_new/logging_hdf5_nonManagerShared.py
+++ source/hilEffort_new/logging_hdf5_nonManagerShared.py
@@ -5,6 +5,11 @@ import os
 import copy
 from multiprocessing import shared_memory
 import numpy as np
+import threading
+import queue
+from line_profiler import LineProfiler
+
+
 
 
 class h5pyWriter(multiprocessing.Process):
@@ -15,47 +20,120 @@ class h5pyWriter(multiprocessing.Process):
         self.mapDict = mapDict
         self.mpSharedMemName = mpSharedMemName
         self.mpSharedMemShape = mpSharedMemShape
+        self.thread_stop_event = threading.Event()
+        self.data_queue = queue.Queue(maxsize=500)
+        
+        self.logHz = 500
+        self.dataBufSize = self.logHz*3
+        
+    # def lineProf (self):
+    #     lp = LineProfiler()
+    #     lp.add_function(self.write_to_hdf)
+    #     lp.run('self.startThread()')
+    #     lp.print_stats()
 
-       
+
+    def startThread(self):
+        self.writer_thread = threading.Thread(target=self.write_to_hdf, daemon=False)
+        self.writer_thread.start()  # Start the writer thread
+
+        self.readData = threading.Thread(target=self.readData, daemon=False)
+        self.readData.start()  # Start the writer thread
+
+        while not self.stop_event.is_set():
+            time.sleep(0.01)
+
+        self.thread_stop_event.set()
+        self.readData.join()
+        self.writer_thread.join()
+
+
+
         # print('in_log:',self.shm,self.shm.buf)
+    def write_to_hdf(self):
+        # os.nice(-10)
+        # os.sched_setaffinity(0, {6})
+        # os.sched_setscheduler(0, os.SCHED_RR, os.sched_param(30))
 
-    def read_and_write_to_hdf5(self):
+        num_channels = len(self.mapDict.keys())
+        print('>>>>>>>>>>>>>Write thread Started')
+        if os.path.exists(self.hdf5FilePath):
+            os.remove(self.hdf5FilePath)
+        with h5py.File(self.hdf5FilePath, 'a') as hdf:
+            # for key in self.mapDict.keys():
+            #     hdf.create_dataset(key, (0,), maxshape=(None,), dtype='f')
+            dataset = hdf.create_dataset('data', (0, num_channels), maxshape=(None, num_channels))
+            dataset.attrs['channel_names'] = np.array(self.mapDict.keys(), dtype='S') 
+            while True:
+                try:
+                    values = self.data_queue.get(block=False)  # Wait for data from the queue
+                except queue.Empty:
+                    time.sleep(0.01)
+                    continue
+                
+                print('self.data_queue>>',self.data_queue.qsize())
+                if values is None:  # Check for the sentinel value to stop
+                    print('>>>>>>>>>>>>>Done Here in write')
+                    break
+            # print('>>>>>>>>>>>>>Write thread Closed')
+                # Write the values to HDF5
+                # for idx, key in enumerate(self.mapDict.keys()):
+                #     dataset = hdf[key]
+                #     dataset.resize((dataset.shape[0] + self.dataBufSize,))
+                #     dataset[-self.dataBufSize:] = values[idx,:]
+                #     # dataset[dataset.shape[0]:dataset.shape[0] + self.dataBufSize] = values[idx,:]
+                dataset.resize((dataset.shape[0] + self.dataBufSize, num_channels))
+                dataset[-self.dataBufSize:] = values    
+                hdf.flush()  # Flush changes to the HDF5 file
+    
+
+    def readData(self):
+        # os.nice(-10)
+        # os.sched_setaffinity(0, {5})
+        os.sched_setscheduler(0, os.SCHED_FIFO, os.sched_param(40))
         shm = shared_memory.SharedMemory(name=self.mpSharedMemName)
         
         mpSharedMem = np.ndarray(self.mpSharedMemShape, dtype='float64', buffer=shm.buf)
 
-        if os.path.exists(self.hdf5FilePath):
-            os.remove(self.hdf5FilePath)
-        with h5py.File(self.hdf5FilePath, 'a') as hdf:
-            for key in self.mapDict.keys():
-                hdf.create_dataset(key, (0,), maxshape=(None,), dtype='f')
+       
             # hdf.create_dataset('systemChannel/systemTime', (0,), maxshape=(None,), dtype='f')
-            values = {f'{key}': [] for key in self.mapDict.keys()}
-            # values['systemChannel/systemTime']=[]
-            buffCounter = 0
-            prevT = time.perf_counter()
-            while not self.stop_event.is_set():
-                # print(mpSharedMem[0])
-                if (time.perf_counter()-prevT)>0.01:
-                    prevT = time.perf_counter()
-                    self.mpLock.acquire()
-                    for key in self.mapDict.keys():
-                        values[key].append(mpSharedMem[self.mapDict[key]['index']])
-                    self.mpLock.release()
-                    buffCounter+=1
-                    
-                    
-                # batch write
-                if buffCounter==100:
-                    # print(values['systemChannel/systemTime'])
-                    for key in values.keys():
-                        dataset = hdf[key]
-                        dataset.resize((dataset.shape[0] + len(values[key]),))
-                        dataset[-len(values[key]):] = values[key]
-                        values[key].clear()
-                    buffCounter = 0
-                    hdf.flush()
-                # print(time.perf_counter())
-                # time.sleep(0.01)  # Simulate cyclic reading every second
-                # print('Log Loop Rate:',1/(time.perf_counter()-prevT))
-                
+            # values = {f'{key}': [] for key in self.mapDict.keys()}
+        values = np.empty((len(self.mapDict.keys()),self.dataBufSize))
+        # values['systemChannel/systemTime']=[]
+        buffCounter = 0
+        lateCounter = 0
+
+        indices = [self.mapDict[key]['index'] for key in self.mapDict.keys()]
+        prevT = time.perf_counter()
+        print('startTime', prevT)
+        while not self.thread_stop_event.is_set():
+            # print(mpSharedMem[0])
+            deltaT =  time.perf_counter()-prevT
+            if deltaT >= (1/self.logHz):
+                # print('>>>>>>>',1/(time.perf_counter()-prevT),totalCounter)
+                if 1/(deltaT) < (self.logHz-5):
+                    lateCounter+=1
+                prevT = time.perf_counter()
+                self.mpLock.acquire() 
+                values[:,buffCounter] = mpSharedMem 
+                values[1,buffCounter] = 1/(deltaT)
+                self.mpLock.release()
+                buffCounter+=1
+            if buffCounter==self.dataBufSize:
+                # print('>>>>>>>>>>Here')
+                self.data_queue.put(values,block=False, timeout=1)
+                # print(values['systemChannel/systemTime'])
+                # for idx, key in enumerate(self.mapDict.keys()):
+                #     dataset = hdf[key]
+                #     dataset.resize((dataset.shape[0] + buffCounter,))
+                #     dataset[-buffCounter:] = values[idx,:]
+                #     # values[key].clear()
+                buffCounter = 0
+                # hdf.flush()
+        
+            # time.sleep(0.0001)
+            # os.sched_yield()
+        self.data_queue.put(values, timeout=1)
+        print("lateCounter>>>>>>>",lateCounter)
+        self.data_queue.put(None)
+  
--- source/hilEffort_new/testnonManagerSharedMem.py
+++ source/hilEffort_new/testnonManagerSharedMem.py
@@ -14,6 +14,12 @@ import utilityScipts
 from logging_hdf5_nonManagerShared import h5pyWriter
 from utilityScipts import channelDictTemplate
 import numpy as np
+import gc
+import os
+from randomgen import ExtendedGenerator, PCG64
+
+# from pynput.keyboard import Key, Controller
+
 
 
 def main():
@@ -21,37 +27,72 @@ def main():
     mapDict = dict()
     mapDict['systemChannel/systemTime']=dict(channelDictTemplate())
     mapDict['systemChannel/systemTime']['index']=0
+    mapDict['systemChannel/logRate']=dict(channelDictTemplate())
+    mapDict['systemChannel/logRate']['index']=1
+
+
+    if platform.system() == "Linux":
+        shm_gen = dp.can_dict('/home/mtitoo/pyHIL/supportFiles/config_testDB.yml',manager)
+    else:
+        shm_gen= dp.can_dict(r'C:\Users\Public\Documents\gitRepoExternal\pyHIL\supportFiles\config_testDB.yml',manager)
     
-    shm_gen= dp.can_dict(r'C:\Users\Public\Documents\gitRepoExternal\pyHIL\supportFiles\config_testDB.yml',manager)
+    # shm_gen= dp.can_dict(r'C:\Users\Public\Documents\gitRepoExternal\pyHIL\supportFiles\config_testDB.yml',manager)
     startIndex = 1
     tempDict = shm_gen.updateDict(startIndex)
-    mapDict = mapDict| tempDict
+    mapDict.update(tempDict)
 
     print('>>>>>>>>>>>>>>>>>>:',len(mapDict))
     return mapDict
     
 
 def injectData(mpSharedMemName,mpSharedMemShape,stop_event,mpLock,mapDict):
+    # os.nice(-9)
+    # os.sched_setaffinity(0, {4})
+    os.sched_setscheduler(0, os.SCHED_FIFO, os.sched_param(99))
+    rng = ExtendedGenerator(PCG64())
     shm = shared_memory.SharedMemory(name=mpSharedMemName)
     mpSharedMem = np.ndarray(mpSharedMemShape, dtype='float64', buffer=shm.buf)
     print('in_update:',shm,shm.buf)
 
     prevT = time.perf_counter()
+    prevTGC = time.perf_counter()
+    lateCounter = 0
+    totalCounter = 0
+    updateHz = 500
+    new_data = np.random.randn(len(mapDict.keys()))
     while not stop_event.is_set():
         deltaT =  time.perf_counter()-prevT
-        if (deltaT) > 0.005:
+        deltaTGC = time.perf_counter() - prevTGC 
+        if (deltaT) >= (1/updateHz):
+            if 1/(deltaT) < (updateHz-5):
+                lateCounter+=1
             prevT = time.perf_counter()
+            # new_data = np.random.randn(len(mapDict.keys()))
             mpLock.acquire()
-            for key in mapDict.keys():
-                mpSharedMem[mapDict[key]['index']] = (random.randint(1,127))
+
+            # for key in mapDict.keys():
+            #     # (random.randint(1,127))
+            #     # pass
+            #     mpSharedMem[mapDict[key]['index']] = 2
+            mpSharedMem[:] = new_data
             mpSharedMem[mapDict['systemChannel/systemTime']['index']] = 1/(deltaT)
+            totalCounter+=1
             mpLock.release()
+    # time.sleep(0.0001)
+    print('>>>>>>>>>>Late Counter', lateCounter,'totalCouter:',totalCounter)
             # print('Update Loop Rate:',1/(time.perf_counter()-prevT))
+
+            
         # time.sleep(0.01)
 
 
 
 if __name__== '__main__':
+    
+    # gc.disable()
+    gc.enable()
+    # os.nice(-10)
+    
     # asyncio.run(main())
     manager = multiprocessing.Manager()
     stop_event = multiprocessing.Event()
@@ -68,19 +109,22 @@ if __name__== '__main__':
 
     hdf5Writer = h5pyWriter('myhdf5.hdf5',shm.name,mpSharedMem.shape,stop_event,mpLock,mapDict)
     inject_process = multiprocessing.Process(target=injectData, args=(shm.name,mpSharedMem.shape,stop_event,mpLock,mapDict))
-    hdf5_process = multiprocessing.Process(target=hdf5Writer.read_and_write_to_hdf5, args=())
-
-    inject_process.start()
+    hdf5_process = multiprocessing.Process(target=hdf5Writer.startThread, args=())
+    
     hdf5_process.start()
+    time.sleep(2)
+    inject_process.start()
+    
 
-    while True:
-    # print_unknown_structure(shared_dict)
-        if keyboard.is_pressed('x'):
-            print("Key 'x' pressed. Stopping the test.")
-            stop_event.set()
-            break
+    # while True:
+    # # print_unknown_structure(shared_dict)
+    #     if keyboard.is_pressed('x'):
+    #         print("Key 'x' pressed. Stopping the test.")
+    #         stop_event.set()
+    #         break
         
-        time.sleep(0.1)  
+    time.sleep(10)  
+    stop_event.set()
     
     inject_process.join()
     hdf5_process.join()
